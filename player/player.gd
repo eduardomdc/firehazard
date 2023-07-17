7173c611790490b71517c061af08b1ec0b2191db
eduardomdc <eduardomc@dcc.ufrj.br>
2023-07-17 20:00:29 -0300

    player olha pro mouse de frente

--- player/player.gd
+++ player/player.gd
@@ -4,13 +4,12 @@ extends CharacterBody2D
 const move_speed = 400.0
 
 func _physics_process(_delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
 		velocity = direction * move_speed
 	else:
 		velocity = Vector2.ZERO
+	
 	move_and_slide()
 	
 	look_at(get_global_mouse_position())
