37c57db1ced1850b94f8c9de185bd8ae90d228e6
eduardomdc <eduardomc@dcc.ufrj.br>
2023-07-17 19:52:10 -0300

    player olha pro mouse

--- player/player.gd
+++ player/player.gd
@@ -1,20 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const move_speed = 400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction:
-		velocity = direction * SPEED
+		velocity = direction * move_speed
 	else:
 		velocity = Vector2.ZERO
-
 	move_and_slide()
+	
+	look_at(get_global_mouse_position())
