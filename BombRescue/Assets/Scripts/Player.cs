5850e80b3037e6d7665e7bb35245272f485ca168
Rafahgol <rafalima245@hotmail.com>
2023-07-19 20:23:45 -0300

    Player sprite, movimento, partícula do gás do extintor
    Adicionei o sprite do player, codigos do movimento até então, partícula que vai servir como gás do extintor e sprite de teste de fumaça

--- BombRescue/Assets/Scripts/Player.cs
+++ BombRescue/Assets/Scripts/Player.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    //public Extintor extintor;
+    Vector2 moveDirection;
+    Vector2 mousePosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX,moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
+
+     private void FixedUpdate()
+     {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x)* Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
+        
+     }
 }
