b8eea278f3c5e1115e5458a704bced25a9f85d52
Ericson Soares <ericson.ds999@gmail.com>
2023-07-28 00:08:33 -0300

    Introduzindo fog do incêndio

--- BombRescue/Assets/Scripts/CbcTorso.cs
+++ BombRescue/Assets/Scripts/CbcTorso.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using MangoFog;
 using UnityEngine;
 
 public class CbcTorso : MonoBehaviour
 {
+    public MangoFogUnit fogUnit;
+
     //Código que faz com que a cabeça e torso do PLayer se movem de acordo com o mouse.
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = new(mousePosition.x - transform.position.x, mousePosition.y
         - transform.position.y);
         transform.up = direction;
+        fogUnit.transform.up = direction;
     }
 }
--- BombRescue/Assets/Scripts/Player.cs
+++ BombRescue/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using MangoFog;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,42 +8,46 @@ public class Player : MonoBehaviour
 {
     //Esse código não ta sendo mais usado no momento mas deixei aqui por precaução 
     public float moveSpeed = 5f;
+
     public Rigidbody2D rb, rig;
+
     //public Extintor extintor;
     public Vector2 moveDirection;
     public Vector2 mousePosition;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-      float moveX = Input.GetAxisRaw("Horizontal");
-      float moveY = Input.GetAxisRaw("Vertical"); 
-      moveDirection = new Vector2(moveX,moveY).normalized;
-      mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var moveX = Input.GetAxisRaw("Horizontal");
+        var moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-     private void FixedUpdate()
-     {
+    private void FixedUpdate()
+    {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x)* Mathf.Rad2Deg - 90f;
+        var aimDirection = mousePosition - rb.position;
+        var aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = aimAngle;
-     }
-     /* void move()
-     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical"); 
-        if (Input.GetAxis("Horizontal") >0f || (Input.GetAxis("Vertical") > 0f))
-        {
-            anim.SetBool("walk", true);
-        }
+    }
+    /* void move()
+    {
+       float moveX = Input.GetAxisRaw("Horizontal");
+       float moveY = Input.GetAxisRaw("Vertical"); 
+       if (Input.GetAxis("Horizontal") >0f || (Input.GetAxis("Vertical") > 0f))
+       {
+           anim.SetBool("walk", true);
+       }
 
-        moveDirection = new Vector2(moveX,moveY).normalized;
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-     } */
+       moveDirection = new Vector2(moveX,moveY).normalized;
+       mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    } */
 }
